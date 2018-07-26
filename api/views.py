c4456bb215b7f1f3b3d4f55102333ffe549c08ca
Abdulmohsen <training.coded@gmail.com>
2018-07-26 15:41:15 +0300

    added normal api request without using requests

--- api/views.py
+++ api/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 # Complete me! I should be your APIListView
 import requests
 from django.http import Http404, JsonResponse
+from django.shortcuts import render, redirect
+
 
 class RestaurantSerializer(serializers.ModelSerializer):
     class Meta:
@@ -21,7 +23,13 @@ class RestaurantListView(views.APIView):
 		return Response(serializer.data)
 
 def get_all_restaurants(request):
-	url = "http://127.0.0.1:8000/api/list/"
+	#url = "http://127.0.0.1:8000/api/list/"
+	url = "https://api.github.com/events"
 	response = requests.get(url)
-	print(response)
-	return JsonResponse(response.json(), safe=False)
+	print(response.json())
+	items = response.json()
+		
+	context = {
+		"github_events": items,
+	}
+	return render(request, 'list.html', context)
